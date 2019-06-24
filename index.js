29bfacb6c0004c098a2bd925604bc16a200ae56f
github username <github email address>
2019-06-24 16:03:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+function findMatching (drivers, string) {
+  let matching_drivers = drivers.filter(name => {
+    return name.toLowerCase() === string.toLowerCase();
+  })
+  return matching_drivers;
+}
+
+function fuzzyMatch (drivers, string) {
+  let matching_drivers = drivers.filter(name => {
+    return name.startsWith(string);
+  })
+  return matching_drivers;
+}
+
+function matchName (drivers, string) {
+  let matching_driver = drivers.filter(driver => {
+    return driver.name.toLowerCase() === string.toLowerCase();
+  })
+  return matching_driver;
+}
